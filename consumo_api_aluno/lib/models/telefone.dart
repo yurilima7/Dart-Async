1afd2509224ef7e22f9fb4b446ac90c3edea97e2
Yuri Lima <61335084+yurilima7@users.noreply.github.com>
2023-02-23 19:19:35 -0300

    Serialização e Deserialização

--- consumo_api_aluno/lib/models/telefone.dart
+++ consumo_api_aluno/lib/models/telefone.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class Telefone {
   int ddd;
   String telefone;
@@ -6,4 +8,33 @@ class Telefone {
     required this.ddd,
     required this.telefone,
   });
+
+  // passo 1 - serialização
+  // toMap(): Pega o objeto (Telefone) e transforma em um Objeto MapMap<String, dynamic>
+  Map<String, dynamic> toMap() {
+    return {
+      'ddd': ddd,
+      'telefone': telefone,
+    };
+  }
+
+  // passo 2 - serialização
+  // toJson: Pega um Map<String, dynamic> e transforma em um pacote dart
+  String toJson() => jsonEncode(toMap());
+
+  // passo 2 - deserialização
+  // transformando o mapa de chave e valor em um telefone
+  factory Telefone.fromMap(Map<String, dynamic> map) {
+    return Telefone(
+      ddd: map['ddd'],
+      telefone: map['telefone'],
+    );
+  }
+
+  // passo 1 - deserialização
+  // recebendo String e transformando em um map de chave e valor
+  factory Telefone.fromJson(String json) {
+    final jsonMap = jsonDecode(json);
+    return Telefone.fromMap(jsonMap);
+  }
 }
